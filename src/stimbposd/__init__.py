ff718690bcccbecda2f285bafc704cf3ad692263
Oscar Higgott <29460323+oscarhiggott@users.noreply.github.com>
2024-02-19 22:37:23 +0000

    Add __version__

--- src/stimbposd/__init__.py
+++ src/stimbposd/__init__.py
@@ -1,3 +1,5 @@
+__version__ = '0.0.dev1'
+
 from stimbposd.bp_osd import BPOSD  # noqa: F401
 from stimbposd.sinter_bp_osd import SinterDecoder_BPOSD, sinter_decoders  # noqa: F401
 from stimbposd.dem_to_matrices import detector_error_model_to_check_matrices  # noqa: F401
